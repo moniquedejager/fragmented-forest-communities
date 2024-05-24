4e228c6a4fab9e0bae9b9343f4ad8925f0c7177c
Monique de Jager <m.dejager@uu.nl>
2024-05-24 16:24:41 +0200

    back to the old model

--- src/simulate_community_dynamics.R
+++ src/simulate_community_dynamics.R
@@ -5,23 +5,6 @@ simulate_community_dynamics <- function(rv){
   # the number of species stabilizes:
   ###################################################################
     
-  # every Pm-value needs a constant to correct for the surface area under the
-  # graph: 
-  subcom <- 1
-  dx <- abs(rv$x - rv$x[rv$comm_ID == subcom])
-  dy <- abs(rv$y - rv$y[rv$comm_ID == subcom])
-  # continuous boundaries:
-  dx[dx > rv$nx/2] <- rv$nx - dx[dx > rv$nx/2]
-  dy[dy > rv$ny/2] <- rv$ny - dy[dy > rv$ny/2]
-  dist <- sqrt(dx^2 + dy^2)
-  sel  <- (dist < 5)
-  sum_Pm <- vector(length = 0)
-  for (Pm in seq(0.05, 1, 0.05)){
-    Pm_weight  <- (2*pi*Pm^2)^-1 * exp(-1*dist[sel]/Pm) 
-    sum_Pm <- c(sum_Pm, sum(Pm_weight))
-  }
-  rv$Pm_C <- 1/sum_Pm
-  
   print(table(rv$Pm))
   
   calc_n_spec <- function(x) {
@@ -38,64 +21,64 @@ simulate_community_dynamics <- function(rv){
     start_time   <- Sys.time()
     rv$origin_ID_t50 <- rv$comm_ID2 
     for (i in 1:50){
-      #print(round(table(rv$Pm)/(rv$n*rv$n_ind), 2))
-      #print(paste('Working on iteration ', i))
-      #df <- data.frame(x=rv$x,
-      #                 y=rv$y,
-      #                 nspec = rv$nspecies,
-      #                 mPm   = tapply(rv$Pm, rv$comm_ID2, mean))
-      #print(ggplot(df, aes(x=x, y=y, fill=nspec)) + geom_raster())
+      # we let each individual reproduce and disperse to a subcommunity:
+      # dispersal depends on the dispersal parameter, which corresponds to 
+      # the species number. To be able to code this efficiently, we make 
+      # 10 dispersal parameter values instead of the entire range (0.1 - 0.9):
+      Pm <- rv$Pm
       
-      # for each subcommunity, we will perform a weighted random sampling of
-      # all individuals to replace the old generation. Weights are based on the
-      # dispersal capacity of the individuals and their distance to the subcom-
-      # munity. 
-      id_replacement <- sapply(rv$comm_ID[rv$comm_type == 'sub'], 
-                               function(subcom){
-                                 dx <- abs(rv$x - rv$x[rv$comm_ID == subcom])
-                                 dy <- abs(rv$y - rv$y[rv$comm_ID == subcom])
-                                 # continuous boundaries:
-                                 dx[dx > rv$nx/2] <- rv$nx - dx[dx > rv$nx/2]
-                                 dy[dy > rv$ny/2] <- rv$ny - dy[dy > rv$ny/2]
-                                 dist <- sqrt(dx^2 + dy^2)
-                                 dist <- unlist(lapply(dist, function(x) 
-                                   rep(x, rv$n_ind)))
-                                 
-                                 sel <- (rv$species > 0)&(dist < 5)
-                                 ids <- (1:length(rv$x2))[sel]
-                                 Pm  <- (2*pi*rv$Pm[sel]^2)^-1 * 
-                                   exp(-1*dist[sel]/rv$Pm[sel])*rv$Pm_C[rv$Pm[sel]/0.05] 
-                                 
-                                 id_replacement <- sample(ids, 
-                                                          rv$n_ind, 
-                                                          prob = Pm, 
-                                                          replace = TRUE)
-                                 return(id_replacement)
-                               })
-      id_replacement_all <- as.vector(id_replacement)
+      # Per Pm value, select all individuals with this value and randomly 
+      # select the subcommunity to reproduce and disperse to and the random
+      # individual to replace in this subcommunity
+      r_comm <- rep(61, rv$tot2) # 61 is the focal subcommunity
+      r_ind  <- sample(1:rv$n_ind, rv$tot2, replace=TRUE)
+      for (iPm in unique(Pm[Pm > 0])) {
+        sel <- Pm == iPm
+        Pm2 <- (2*pi*iPm^2)^-1 * exp(-1*rv$dist / iPm) 
+        Pm2 <- Pm2 / sum(Pm2)
+        r_comm[sel] <- sample(1:121, sum(sel), replace=TRUE, prob=Pm2)
+      }
+      x_comm   <- rv$x2 + rv$dx[r_comm]
+      y_comm   <- rv$y2 + rv$dy[r_comm]
       
-      new_species  <- rv$species[id_replacement_all]
-      new_Pm       <- rv$Pm[id_replacement_all]
-      origin_ID_t1 <- rv$comm_ID2[id_replacement_all]
-      origin_ID_t50<- rv$origin_ID_t50[id_replacement_all]
+      x_comm <- x_comm - rv$nx*(x_comm > rv$nx) + rv$nx*(x_comm < 1)
+      y_comm <- y_comm - rv$ny*(y_comm > rv$ny) + rv$ny*(y_comm < 1)
+      r_commID <- (y_comm - 1)*rv$nx + x_comm
       
-      rv$species[rv$comm_type2 == 'sub']       <- new_species
-      rv$Pm[rv$comm_type2 == 'sub']            <- new_Pm
-      rv$origin_ID_t1[rv$comm_type2 == 'sub']  <- origin_ID_t1
-      rv$origin_ID_t50[rv$comm_type2 == 'sub'] <- origin_ID_t50
- 
+      # and in a random order (otherwise, the last individual always replaces 
+      # an earlier dispersing individual)
+      sel <- rv$species > 0
+      a <- sample((1:rv$tot2)[sel], sum(sel), replace=F)
+
+      spec     <- rv$species
+      ind_id   <- ((r_commID - 1) * rv$n_ind + r_ind)      
+      spec[ind_id[a]] <- rv$species[a]
+      Pm       <- rv$Pm
+      Pm[ind_id[a]] <- rv$Pm[a]
+      rv$species <- spec
+      rv$Pm      <- Pm
+      
       # add mutations:
-      mutated             <- (runif(length(rv$species), 0, 1) < rv$mutation_rate)*1
+      mutated                  <- runif(length(rv$species), 0, 1) < rv$mutation_rate
       mutated[rv$species == 0] <- 0
       if (sum(mutated) > 0){
         new_species         <- max(rv$species) + 1:sum(mutated)
-        #new_Pm              <- rv$Pm[mutated == 1] + sample(seq((-1*rv$max_mutation),rv$max_mutation, 0.05), sum(mutated), replace=TRUE) 
-        new_Pm              <- rv$Pm[mutated == 1] + runif(sum(mutated), -1*rv$max_mutation,rv$max_mutation) 
+        new_Pm              <- rv$Pm[mutated] + sample(seq((-1*rv$max_mutation),rv$max_mutation, 0.05), sum(mutated), replace=TRUE) 
         new_Pm[new_Pm < 0.05] <- 0.05
         
-        rv$species[mutated == 1] <- new_species
-        rv$Pm[mutated == 1]      <- new_Pm
+        rv$species[mutated] <- new_species
+        rv$Pm[mutated]      <- new_Pm
       }
+      rv$species[rv$comm_type2 != 'sub'] <- 0
+      rv$Pm[rv$comm_type2 != 'sub'] <- 0
+      
+      origin_ID_t50 <- rv$origin_ID_t50
+      rv$origin_ID_t50[ind_id[a]] <- 
+        origin_ID_t50[a]
+      
+      origin_ID_t1 <- rv$comm_ID2
+      rv$origin_ID_t1[ind_id[a]] <- 
+        origin_ID_t1[a]
       
       rv$nspecies   <- sapply(rv$comm_ID, calc_n_spec)
       mn            <- mean(rv$nspecies[rv$comm_type =='sub'])
@@ -103,7 +86,8 @@ simulate_community_dynamics <- function(rv){
     }
     end_time <- Sys.time()
     print(end_time - start_time)
-    print(round(hist(rv$Pm, breaks = seq(-0.025, 1.025, 0.05))$counts / (rv$n*rv$n_ind), 3))
+    h <- hist(rv$Pm[rv$comm_type2 == 'sub'], breaks = seq(-0.025, 1.025, 0.05))
+    print(round(h$counts / sum(h$counts), 3))
     
     x <- 1:50
     s <- summary(lm(mean_nspecies[length(mean_nspecies) - 49:0]~x))
